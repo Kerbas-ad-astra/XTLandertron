395343dc0fea78a954cbd5534c31a27825cb9000
charfa <bartlomiej@chalfen.pl>
2015-11-08 01:16:10 +0100

    Release 0.10.0.3

--- Landertron/source/Properties/AssemblyInfo.cs
+++ Landertron/source/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("0.10.0")]
-[assembly: AssemblyFileVersion("0.10.0.2")]
+[assembly: AssemblyFileVersion("0.10.0.3")]
 [assembly: AssemblyInformationalVersion("0.10.0")]
 
 [assembly: KSPAssembly("Landertron", 0, 10)]
